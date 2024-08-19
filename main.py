f2452fbb8bf33ae1e6a0e864902d1e0f3bcd6970
rafaelrmb <rafaelmotermb@gmail.com>
2024-08-19 19:49:41 -0300

    feat: add functions to sort the dictionary by value and generate a report on chars found on book

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main ():
   num_of_words = count_total_of_words(book_text)
   chars_dict = count_total_chars(book_text)
   print(f"{num_of_words} words in the book!")
-  print(chars_dict)
+  generate_report(chars_dict, path)
 
 def get_book_as_string(path_to_book):
   with open(path_to_book) as book_file:
@@ -25,4 +25,21 @@ def count_total_chars(text):
         else: 
           chars_dict[char] = 1
   return chars_dict
+
+def generate_report(dictionary, path):
+  sorted_dictionary = sort_dictionary_by_value(dictionary)
+
+  print(f"--- Begin report of {path} ---")
+
+  for item in sorted_dictionary:
+    print(f"The char {item[0]} was found {item[1]} times")
+
+  print("--- End report ---")
+
+def sort_dictionary_by_value(dictionary):
+  dictionary_tuple = dictionary.items()
+  sorted_items_by_value = sorted(dictionary_tuple, key=lambda item: item[1], reverse=True)
+  
+  return sorted_items_by_value
+
 main()
